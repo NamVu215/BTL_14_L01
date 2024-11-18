6b46a53a717ca22f4702a76dbbd93f43c610c27d
TonyBM0907 <nnamvu02@gmail.com>
2024-11-18 21:12:15 +0700

    Final version

--- test.py
+++
@@ -1,4 +0,0 @@
-MOVIES_FILE = 'C:/Users/TonyZ/Desktop/BTL ML/ML14/ml-latest-small/movies.csv'
-RATINGS_FILE = 'C:/Users/TonyZ/Desktop/BTL ML/ML14/ml-latest-small/ratings.csv'
-TAGS_FILE = 'C:/Users/TonyZ/Desktop/BTL ML/ML14/ml-latest-small/tags.csv'
-LINKS_FILE = 'C:/Users/TonyZ/Desktop/BTL ML/ML14/ml-latest-small/links.csv'
